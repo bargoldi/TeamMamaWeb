e2879e18f7218f89f195f0ed575c910cf8b92a82
orihpt <52978143+orihpt@users.noreply.github.com>
2024-10-27 12:09:31 +0200

    Update

--- main.dart.js
+++ main.dart.js
@@ -53496,7 +53496,7 @@ A.c_k.prototype={
 $1(a){var s=A.ko().b
 if(s==null)s=null
 else{s=s.canvasKitBaseUrl
-if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/36335019a8eab588c3c2ea783c618d90505be233/":s)+a},
+if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/db49896cf25ceabc44096d5f088d86414e05a7aa/":s)+a},
 $S:37}
 A.aTG.prototype={
 hi(a){B.d.aM(this.a.a.save())},
