edb12246c2141860b89d1fe186a4632be56a6b9f
orihpt <52978143+orihpt@users.noreply.github.com>
2024-10-29 15:58:28 +0200

    Update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1930040407"
+    serviceWorkerVersion: "4153364531"
   }
 });
